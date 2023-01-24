0ae82a7b27b8b95e6e59de40149e4c2d32827f0d
Dom-Caracappa <fknbab@pm.me>
2023-01-23 17:21:51 -0800

    CW: Ready to deploy

--- app.py
+++ app.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/page1')
-def page1():
-    return render_template('page1.html')
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
 
-@app.route('/page2')
-def page2():
-    return render_template('page2.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
-@app.route('/page3')
-def page3():
-    return render_template('page3.html')
+@app.route('/steal-this-website')
+def steal():
+    return render_template('steal.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
